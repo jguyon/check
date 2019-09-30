b8ea7b31736e097ff9c2d3f517a7d558ce625d22
Jérôme Guyon <jeromep.guyon@gmail.com>
2019-09-30 19:03:58 +0200

    Remove argument check for error function

--- src/error.js
+++ src/error.js
@@ -1,8 +1,4 @@
-import invariant from "tiny-invariant";
-
 export default function error(message, path = []) {
-  invariant(Array.isArray(path), "expected path argument to be an array");
-
   return {
     isOk: false,
     path,
