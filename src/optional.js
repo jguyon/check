d38b0e9cc09eac5b2c5387e947802c1fb23950e9
Jérôme Guyon <jeromep.guyon@gmail.com>
2019-10-07 18:21:06 +0200

    Add support for refs in optional check

--- src/optional.js
+++ src/optional.js
@@ -1,15 +1,18 @@
 import _ from "lodash";
 import invariant from "tiny-invariant";
+import withRefs, { hasRefs } from "./withRefs";
 import ok from "./ok";
 
 export default function optional(check) {
   invariant(_.isFunction(check), "expected check argument to be a function");
 
-  return value => {
+  return withRefs(hasRefs(check) ? check.refs : [], (value, refs) => {
     if (_.isNil(value)) {
       return ok(null);
+    } else if (hasRefs(check)) {
+      return check.check(value, refs);
     } else {
       return check(value);
     }
-  };
+  });
 }
--- test/optional.test.js
+++ test/optional.test.js
@@ -1,8 +1,29 @@
-import { optional, string, trim } from "../src";
+import {
+  optional,
+  string,
+  trim,
+  minLength,
+  ref,
+  checkWithRefs,
+  ok,
+} from "../src";
 
 test("check succeeds when value is present and given check succeeds", () => {
   const check = optional(trim());
   const result = check("  jerome   ");
+
+  expect(result).toEqual({
+    isOk: true,
+    value: "jerome",
+  });
+});
+
+test("check succeeds when value is present and given check with refs succeeds", () => {
+  const check = optional(minLength(ref(["min"])));
+  const result = checkWithRefs(check, "jerome", {
+    path: ["min"],
+    result: ok(4),
+  });
 
   expect(result).toEqual({
     isOk: true,
@@ -21,4 +42,55 @@ test("check succeeds when value is not present", () => {
       value: null,
     });
   }
+});
+
+test("check succeeds when value is not present with check with refs", () => {
+  const check = optional(minLength(ref(["min"])));
+
+  for (const value of [null, undefined]) {
+    const result = checkWithRefs(check, value, {
+      path: ["min"],
+      result: ok(4),
+    });
+
+    expect(result).toEqual({
+      isOk: true,
+      value: null,
+    });
+  }
+});
+
+test("check fails when value is present and given check fails", () => {
+  const check = optional(minLength(8));
+  const result = check("jerome");
+
+  expect(result).toEqual({
+    isOk: false,
+    errors: [
+      {
+        path: [],
+        value: "jerome",
+        message: "is too short",
+      },
+    ],
+  });
+});
+
+test("check fails when value is present and given check with refs fails", () => {
+  const check = optional(minLength(ref(["min"])));
+  const result = checkWithRefs(check, "jerome", {
+    path: ["min"],
+    result: ok(8),
+  });
+
+  expect(result).toEqual({
+    isOk: false,
+    errors: [
+      {
+        path: [],
+        value: "jerome",
+        message: "is too short",
+      },
+    ],
+  });
 });
