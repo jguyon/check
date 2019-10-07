cc7ccf44c4f8c4b6eb90c9a47b7534960afd0e50
Jérôme Guyon <jeromep.guyon@gmail.com>
2019-10-07 18:07:34 +0200

    Add support for refs in pipe check

--- src/pipe.js
+++ src/pipe.js
@@ -1,5 +1,6 @@
 import _ from "lodash";
 import invariant from "tiny-invariant";
+import withRefs, { hasRefs } from "./withRefs";
 import ok from "./ok";
 
 export default function pipe(...checks) {
@@ -8,11 +9,23 @@ export default function pipe(...checks) {
     "expected all arguments to be functions",
   );
 
-  return value => {
+  const refs = getRefsFromChecks(checks);
+
+  return withRefs(refs, (value, refs) => {
     let output = value;
+    let refI = 0;
 
     for (let i = 0; i < checks.length; i++) {
-      const result = checks[i](output);
+      const check = checks[i];
+
+      let result;
+      if (hasRefs(check)) {
+        const checkRefs = _.slice(refs, refI, refI + check.refs.length);
+        refI += check.refs.length;
+        result = check.check(output, checkRefs);
+      } else {
+        result = check(output);
+      }
 
       if (result.isOk) {
         output = result.value;
@@ -22,5 +35,10 @@ export default function pipe(...checks) {
     }
 
     return ok(output);
-  };
+  });
+}
+
+function getRefsFromChecks(checks) {
+  const refArrays = _.map(checks, check => (hasRefs(check) ? check.refs : []));
+  return _.flatten(refArrays);
 }
--- test/pipe.test.js
+++ test/pipe.test.js
@@ -1,4 +1,14 @@
-import { pipe, string, trim, minLength, pattern } from "../src";
+import {
+  pipe,
+  string,
+  trim,
+  minLength,
+  maxLength,
+  pattern,
+  ref,
+  checkWithRefs,
+  ok,
+} from "../src";
 
 test("check succeeds when all given checks succeed", () => {
   const check = pipe(
@@ -7,6 +17,32 @@ test("check succeeds when all given checks succeed", () => {
     minLength(4),
   );
   const result = check("  jerome   ");
+
+  expect(result).toEqual({
+    isOk: true,
+    value: "jerome",
+  });
+});
+
+test("check succeeds when all given checks with refs succeed", () => {
+  const check = pipe(
+    string(),
+    trim(),
+    minLength(ref(["min"])),
+    maxLength(ref(["max"])),
+  );
+  const result = checkWithRefs(
+    check,
+    "  jerome   ",
+    {
+      path: ["min"],
+      result: ok(4),
+    },
+    {
+      path: ["max"],
+      result: ok(8),
+    },
+  );
 
   expect(result).toEqual({
     isOk: true,
@@ -32,6 +68,30 @@ test("check fails with error from first failing check", () => {
     pattern(/^[A-Z]/),
   );
   const result = check("     jerome           ");
+
+  expect(result).toEqual({
+    isOk: false,
+    errors: [
+      {
+        path: [],
+        value: "jerome",
+        message: "is too short",
+      },
+    ],
+  });
+});
+
+test("check fails with error from first failing check with refs", () => {
+  const check = pipe(
+    string(),
+    trim(),
+    minLength(ref(["min"])),
+    pattern(/^[A-Z]/),
+  );
+  const result = checkWithRefs(check, "   jerome      ", {
+    path: ["min"],
+    result: ok(8),
+  });
 
   expect(result).toEqual({
     isOk: false,
