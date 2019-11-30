baea1703870bce66ae26704c0b19d3e8e7997e10
Jérôme Guyon <jeromep.guyon@gmail.com>
2019-11-30 15:04:16 +0100

    Make isRef part of the public API

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ export { default as ok } from "./ok";
 export { default as error } from "./error";
 export { default as errors } from "./errors";
 export { default as up } from "./up";
-export { default as ref } from "./ref";
+export { default as ref, isRef } from "./ref";
 export { default as withRefs } from "./withRefs";
 export { default as any } from "./any";
 export { default as test } from "./test";
