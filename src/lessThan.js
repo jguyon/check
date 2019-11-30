37834406db63f1918300dea36e77b8f0aa6fca07
Jérôme Guyon <jeromep.guyon@gmail.com>
2019-11-30 15:21:55 +0100

    Add support for refs in lessThan check

--- src/lessThan.js
+++ src/lessThan.js
@@ -1,9 +1,14 @@
 import _ from "lodash";
 import invariant from "tiny-invariant";
+import { isRef } from "./ref";
+import withRefs from "./withRefs";
 import test from "./test";
 
 export default function lessThan(max, message = "is too high") {
-  invariant(_.isFinite(max), "expected max argument to be a valid number");
+  invariant(
+    _.isFinite(max) || isRef(max),
+    "expected max argument to be a valid number or a ref",
+  );
 
-  return test(value => value < max, message);
+  return withRefs([max], test((value, max) => value < max, message));
 }
