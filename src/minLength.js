d125e283b8b0e207fcd00dd88f6b13aacb95f781
Jérôme Guyon <jeromep.guyon@gmail.com>
2019-11-30 15:03:53 +0100

    Add support for refs in minLength check

--- src/minLength.js
+++ src/minLength.js
@@ -1,9 +1,14 @@
 import _ from "lodash";
 import invariant from "tiny-invariant";
+import { isRef } from "./ref";
+import withRefs from "./withRefs";
 import test from "./test";
 
 export default function minLength(min, message = "is too short") {
-  invariant(_.isFinite(min), "expected min argument to be a valid number");
+  invariant(
+    _.isFinite(min) || isRef(min),
+    "expected min argument to be a valid number or a ref",
+  );
 
-  return test(value => value.length >= min, message);
+  return withRefs([min], test((value, min) => value.length >= min, message));
 }
--- test/minLength.test.js
+++ test/minLength.test.js
@@ -1,4 +1,4 @@
-import { minLength } from "../src";
+import { minLength, ref } from "../src";
 
 test("check succeeds when given value is long enough", () => {
   const check = minLength(3);
@@ -18,6 +18,27 @@ test("check fails when given value is too short", () => {
   const result = check([1, 2, 3]);
 
   expect(result).toEqual({
+    isOk: false,
+    errors: [
+      {
+        path: [],
+        value: [1, 2, 3],
+        message: "is too short",
+      },
+    ],
+  });
+});
+
+test("refs are supported for min value", () => {
+  const check = minLength(ref(["min"]));
+  const greaterResult = check([1, 2, 3], { min: 2 });
+  const lesserResult = check([1, 2, 3], { min: 4 });
+
+  expect(greaterResult).toEqual({
+    isOk: true,
+    value: [1, 2, 3],
+  });
+  expect(lesserResult).toEqual({
     isOk: false,
     errors: [
       {
