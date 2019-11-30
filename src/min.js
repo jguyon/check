dee1201b53484fbb3479ddbac660bbe833a53824
Jérôme Guyon <jeromep.guyon@gmail.com>
2019-11-30 15:13:02 +0100

    Add support for refs in min check

--- src/min.js
+++ src/min.js
@@ -1,9 +1,14 @@
 import _ from "lodash";
 import invariant from "tiny-invariant";
+import { isRef } from "./ref";
+import withRefs from "./withRefs";
 import test from "./test";
 
 export default function min(min, message = "is too low") {
-  invariant(_.isFinite(min), "expected min argument to be a valid number");
+  invariant(
+    _.isFinite(min) || isRef(min),
+    "expected min argument to be a valid number or a ref",
+  );
 
-  return test(value => value >= min, message);
+  return withRefs([min], test((value, min) => value >= min, message));
 }
