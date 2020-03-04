de08f7a451044f17ade301572fc03d7eaf4bb288
Jérôme Guyon <jeromep.guyon@gmail.com>
2020-03-04 15:06:33 +0100

    Document ok function

--- src/ok.js
+++ src/ok.js
@@ -1,3 +1,12 @@
+/**
+ * Creates a valid result.
+ *
+ * @param {any} value a valid value
+ * @returns {Object} a valid result wrapping the value.
+ *
+ * @example
+ * C.ok(42); // => { isOk: true, value: 42 }
+ */
 export default function ok(value) {
   return {
     isOk: true,
