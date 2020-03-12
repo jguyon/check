c40646e4e1dca92ce08761e6be538173f852bda1
Jérôme Guyon <jeromep.guyon@gmail.com>
2020-03-12 16:23:46 +0100

    Document toUpper function

--- src/toUpper.js
+++ src/toUpper.js
@@ -1,6 +1,16 @@
 import _ from "lodash";
 import transform from "./transform";
 
+/**
+ * Creates a check function that converts a string to upper case.
+ *
+ * @returns {Function} a check function.
+ *
+ * @example
+ * const check = C.toUpper();
+ *
+ * check("jerome"); // => { isOk: true, value: "JEROME" }
+ */
 export default function toUpper() {
   return transform(value => _.toUpper(value));
 }
