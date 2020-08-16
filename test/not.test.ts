2c4c8e8db4a7fbb8dc3e104cd8f2ee81f310d88a
Jérôme Guyon <jeromep.guyon@gmail.com>
2020-08-16 21:59:56 +0200

    Use mocks in tests for not function

--- test/not.test.ts
+++ test/not.test.ts
@@ -1,7 +1,7 @@
 import * as check from "../src";
 
 test("check succeeds when child check fails", () => {
-  const checkValue = check.not(check.is(42));
+  const checkValue = check.not(() => check.error("value", "is wrong"));
   const result = checkValue(43);
 
   expect(result).toEqual({
@@ -11,7 +11,7 @@ test("check succeeds when child check fails", () => {
 });
 
 test("check fails when child check succeeds", () => {
-  const checkValue = check.not(check.is(42));
+  const checkValue = check.not(() => check.ok("value"));
   const result = checkValue(42);
 
   expect(result).toEqual({
@@ -22,13 +22,31 @@ test("check fails when child check succeeds", () => {
   });
 });
 
+test("child check is called with given value", () => {
+  const checkChild = jest.fn(() => check.ok("value"));
+  const checkValue = check.not(checkChild);
+  checkValue(42);
+
+  expect(checkChild).toHaveBeenCalledTimes(1);
+  expect(checkChild).toHaveBeenCalledWith(42);
+});
+
+test("child check is called with the additional arguments", () => {
+  const checkChild = jest.fn(() => check.ok("value"));
+  const checkValue = check.not<unknown, unknown[]>(checkChild);
+  checkValue(42, "one", "two");
+
+  expect(checkChild).toHaveBeenCalledTimes(1);
+  expect(checkChild).toHaveBeenCalledWith(expect.anything(), "one", "two");
+});
+
 test("given error is returned with the invalid result", () => {
-  const checkValue = check.not(check.is(42), "is 42");
+  const checkValue = check.not(() => check.ok("value"), "is wrong");
   const result = checkValue(42);
 
   expect(result).toEqual({
     isOk: false,
-    error: "is 42",
+    error: "is wrong",
     invalidValue: 42,
     path: [],
   });
@@ -36,51 +54,45 @@ test("given error is returned with the invalid result", () => {
 
 test("given path and invalid value are returned with the invalid result", () => {
   const checkValue = check.not(
-    check.test(({ value }) => value === 42),
+    () => check.ok("value"),
     "is invalid",
     ["value"],
-    ({ value }) => value,
+    () => "invalid value",
   );
-  const result = checkValue({ value: 42 });
+  const result = checkValue(42);
 
   expect(result).toEqual({
     isOk: false,
     error: "is invalid",
-    invalidValue: 42,
+    invalidValue: "invalid value",
     path: ["value"],
   });
 });
 
-test("additional arguments are passed to the child check", () => {
+test("given function to get the invalid value is called with the validated value", () => {
+  const getInvalidValue = jest.fn(() => "invalid value");
   const checkValue = check.not(
-    check.test(
-      (value, ...args) =>
-        args.length === 2 && args[0] === "one" && args[1] === "two",
-    ),
+    () => check.ok("value"),
+    "is invalid",
+    ["value"],
+    getInvalidValue,
   );
-  const result = checkValue(42, "one", "two");
+  checkValue(42);
 
-  expect(result).toEqual({
-    isOk: false,
-    error: "is invalid",
-    invalidValue: 42,
-    path: [],
-  });
+  expect(getInvalidValue).toHaveBeenCalledTimes(1);
+  expect(getInvalidValue).toHaveBeenCalledWith(42);
 });
 
-test("additional arguments are passed to the function to get the invalid value", () => {
-  const checkValue = check.not(
-    check.is(42),
+test("given function to get the invalid value is called with the additional arguments", () => {
+  const getInvalidValue = jest.fn(() => "invalid value");
+  const checkValue = check.not<unknown, unknown[]>(
+    () => check.ok("value"),
     "is invalid",
-    [],
-    (value, ...args: unknown[]) => args,
+    ["value"],
+    getInvalidValue,
   );
-  const result = checkValue(42, "one", "two");
+  checkValue(42, "one", "two");
 
-  expect(result).toEqual({
-    isOk: false,
-    error: "is invalid",
-    invalidValue: ["one", "two"],
-    path: [],
-  });
+  expect(getInvalidValue).toHaveBeenCalledTimes(1);
+  expect(getInvalidValue).toHaveBeenCalledWith(expect.anything(), "one", "two");
 });
