9039c0c0eba8dc34f2792143c6d174f3621af1a3
Jérôme Guyon <jeromep.guyon@gmail.com>
2020-08-16 19:09:41 +0200

    Use mocks in tests for optional function

--- test/optional.test.ts
+++ test/optional.test.ts
@@ -1,32 +1,32 @@
 import * as check from "../src";
 
 test("check succeeds when given value is present and child check succeeds", () => {
-  const checkValue = check.optional(check.toString());
+  const checkValue = check.optional(() => check.ok("value"));
 
   for (const value of ["jerome", "", 42, 0, null]) {
     const result = checkValue(value);
 
     expect(result).toEqual({
       isOk: true,
-      value: value === null ? "" : String(value),
+      value: "value",
     });
   }
 });
 
 test("check fails when given value is present and child check fails", () => {
-  const checkValue = check.optional(check.string());
+  const checkValue = check.optional(() => check.error("value", "is wrong"));
   const result = checkValue(42);
 
   expect(result).toEqual({
     isOk: false,
-    error: "is not a string",
-    invalidValue: 42,
+    error: "is wrong",
+    invalidValue: "value",
     path: [],
   });
 });
 
 test("check succeeds when given value is not present", () => {
-  const checkValue = check.optional(check.string());
+  const checkValue = check.optional(() => check.error("value", "is wrong"));
   const result = checkValue(undefined);
 
   expect(result).toEqual({
@@ -35,12 +35,28 @@ test("check succeeds when given value is not present", () => {
   });
 });
 
-test("additional arguments are passed to the child check", () => {
-  const checkValue = check.optional((value, ...args) => check.ok(args));
-  const result = checkValue(42, "one", "two");
+test("child check is called with given value", () => {
+  const checkChild = jest.fn(() => check.ok("value"));
+  const checkValue = check.optional(checkChild);
+  checkValue(42);
 
-  expect(result).toEqual({
-    isOk: true,
-    value: ["one", "two"],
-  });
+  expect(checkChild).toHaveBeenCalledTimes(1);
+  expect(checkChild).toHaveBeenCalledWith(42);
+});
+
+test("child check is called with the additional arguments", () => {
+  const checkChild = jest.fn(() => check.ok("value"));
+  const checkValue = check.optional<unknown, unknown, unknown[]>(checkChild);
+  checkValue(42, "one", "two");
+
+  expect(checkChild).toHaveBeenCalledTimes(1);
+  expect(checkChild).toHaveBeenCalledWith(expect.anything(), "one", "two");
+});
+
+test("child check is not called when given value is not present", () => {
+  const checkChild = jest.fn(() => check.ok("value"));
+  const checkValue = check.optional(checkChild);
+  checkValue(undefined);
+
+  expect(checkChild).not.toHaveBeenCalled();
 });
