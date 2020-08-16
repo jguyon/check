57d17c9f2caf4c8fd9fc5d4c72b56900eaa32ba0
Jérôme Guyon <jeromep.guyon@gmail.com>
2020-08-16 18:50:33 +0200

    Use mocks in tests for transformAsync function

--- test/transformAsync.test.ts
+++ test/transformAsync.test.ts
@@ -1,21 +1,31 @@
 import * as check from "../src";
 
 test("check succeeds with transformed value", async () => {
-  const checkValue = check.transformAsync(async (value) => String(value));
+  const checkValue = check.transformAsync(async () => "jerome");
   const result = await checkValue(42);
 
   expect(result).toEqual({
     isOk: true,
-    value: "42",
+    value: "jerome",
   });
 });
 
-test("additional arguments are passed to the transform function", async () => {
-  const checkValue = check.transformAsync(async (value, ...args) => args);
-  const result = await checkValue(42, "one", "two");
+test("given transform function is called with the value to transform", async () => {
+  const transform = jest.fn(async () => "jerome");
+  const checkValue = check.transformAsync(transform);
+  await checkValue(42);
 
-  expect(result).toEqual({
-    isOk: true,
-    value: ["one", "two"],
-  });
+  expect(transform).toHaveBeenCalledTimes(1);
+  expect(transform).toHaveBeenCalledWith(42);
+});
+
+test("given transform function is called with the additional arguments", async () => {
+  const transform = jest.fn(async () => "jerome");
+  const checkValue = check.transformAsync<unknown, unknown, unknown[]>(
+    transform,
+  );
+  await checkValue(42, "one", "two");
+
+  expect(transform).toHaveBeenCalledTimes(1);
+  expect(transform).toHaveBeenCalledWith(expect.anything(), "one", "two");
 });
