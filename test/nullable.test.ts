93b5f67c7656930147c7b05ec53399c34508b0f0
Jérôme Guyon <jeromep.guyon@gmail.com>
2020-08-16 20:33:17 +0200

    Use mocks in tests for nullable function

--- test/nullable.test.ts
+++ test/nullable.test.ts
@@ -1,32 +1,32 @@
 import * as check from "../src";
 
 test("check succeeds when given value is not null and child check succeeds", () => {
-  const checkValue = check.nullable(check.toString());
+  const checkValue = check.nullable(() => check.ok("value"));
 
   for (const value of ["jerome", "", 42, 0, undefined]) {
     const result = checkValue(value);
 
     expect(result).toEqual({
       isOk: true,
-      value: value === undefined ? "" : String(value),
+      value: "value",
     });
   }
 });
 
 test("check fails when given value is not null and child check fails", () => {
-  const checkValue = check.nullable(check.string());
+  const checkValue = check.nullable(() => check.error("value", "is wrong"));
   const result = checkValue(42);
 
   expect(result).toEqual({
     isOk: false,
-    error: "is not a string",
-    invalidValue: 42,
+    error: "is wrong",
+    invalidValue: "value",
     path: [],
   });
 });
 
 test("check succeeds when given value is null", () => {
-  const checkValue = check.nullable(check.string());
+  const checkValue = check.nullable(() => check.error("value", "is wrong"));
   const result = checkValue(null);
 
   expect(result).toEqual({
@@ -35,12 +35,28 @@ test("check succeeds when given value is null", () => {
   });
 });
 
-test("additional arguments are passed to the child check", () => {
-  const checkValue = check.nullable((value, ...args) => check.ok(args));
-  const result = checkValue(42, "one", "two");
+test("child check is called with given value", () => {
+  const checkChild = jest.fn(() => check.ok("value"));
+  const checkValue = check.nullable(checkChild);
+  checkValue(42);
 
-  expect(result).toEqual({
-    isOk: true,
-    value: ["one", "two"],
-  });
+  expect(checkChild).toHaveBeenCalledTimes(1);
+  expect(checkChild).toHaveBeenCalledWith(42);
+});
+
+test("child check is called with the additional arguments", () => {
+  const checkChild = jest.fn(() => check.ok("value"));
+  const checkValue = check.nullable<unknown, unknown, unknown[]>(checkChild);
+  checkValue(42, "one", "two");
+
+  expect(checkChild).toHaveBeenCalledTimes(1);
+  expect(checkChild).toHaveBeenCalledWith(expect.anything(), "one", "two");
+});
+
+test("child check is not called when given value is null", () => {
+  const checkChild = jest.fn(() => check.ok("value"));
+  const checkValue = check.nullable(checkChild);
+  checkValue(null);
+
+  expect(checkChild).not.toHaveBeenCalled();
 });
