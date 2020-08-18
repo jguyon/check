4a95afbf8930c5d0d116d335ceb9b27a6c45e685
Jérôme Guyon <jeromep.guyon@gmail.com>
2020-08-18 19:32:20 +0200

    Export async check types

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ export { default as Result } from "./result";
 export { default as ok, OkResult } from "./ok";
 export { default as error, ErrorResult } from "./error";
 export { default as Check } from "./check";
+export { default as AsyncCheck } from "./asyncCheck";
+export { default as MaybeAsyncCheck } from "./maybeAsyncCheck";
 
 export { default as pass } from "./pass";
 export { default as fail } from "./fail";
